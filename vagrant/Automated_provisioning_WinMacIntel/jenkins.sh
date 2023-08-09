95073bf9527de4879b314ed00f6f51bea1e4eca9
Syed Devops <abdulazeem.az@gmail.com>
2023-08-09 12:48:30 +0530

    add jenkins

---
+++ vagrant/Automated_provisioning_WinMacIntel/jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade -y
+# Add required dependencies for the jenkins package
+sudo yum install java-17-openjdk -y
+sudo yum install maven -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
